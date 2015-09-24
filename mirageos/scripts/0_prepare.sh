a39c51dbd0aa503ebd4ddc8ff2018c73c3b8e8cb
Matt Bajor <mbajor@rallydev.com>
2015-09-23 20:35:32 -0500

    Update and clean up scripts

--- mirageos/scripts/0_prepare.sh
+++ mirageos/scripts/0_prepare.sh
@@ -1,17 +1,36 @@
 #!/bin/bash -e
 
+# Install apt tools
 sudo apt-get install -y software-properties-common
+
+# Add the OCaml PPA
 sudo add-apt-repository -y ppa:avsm/ppa
-sudo apt-get update && sudo apt-get install -y curl build-essential m4 ocaml opam libgmp-dev libpcre3-dev libxen-dev pkg-config
 
+# Install OCaml / Opam
+sudo apt-get update
+sudo apt-get install -y curl build-essential m4 ocaml opam \
+  libgmp-dev libpcre3-dev libxen-dev pkg-config
+
+# Init Opam
 opam init -y
-opam switch 4.01.0 -y
-opam update -y
-opam install depext -y
-opam depext xenctrl libvirt xen-api-client
-opam install mirage -y
-opam install jitsu -y
 
+# Switch to OCaml 4.01.0
+opam switch 4.01.0 -y
+
+# Update opam
+opam update -y
+
+# Install our dependency manager
+opam install depext -y
+
+# Install our deps
+opam depext xenctrl libvirt xen-api-client
+
+# Install the mirage package (unikernel packaging tool)
+opam install mirage -y
+
+# Install jitsu (Just In Time Summoning of Unikernels)
+opam install jitsu -y
 
 echo '#####   Run this to activate Mirage! ######'
 echo 'eval `opam config env`'
--- mirageos/scripts/2_jitsu.sh
+++ mirageos/scripts/2_jitsu.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+# Start the daemon with a single record
+# Additional records can be added by just duplicating lines 10-15
 sudo /home/vagrant/.opam/4.01.0/bin/jitsu \
 --bind=0.0.0.0 \
 --forwarder=8.8.8.8 \
@@ -9,3 +13,4 @@ kernel=../myblog/_mirage/mir-www.xen,\
 memory=64000,\
 name=www,\
 nic=xenbr0
+
